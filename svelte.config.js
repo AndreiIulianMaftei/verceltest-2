583907ab2b06b87c0f5165b092a6d7e2fe28c2c4
AndreiIulianMaftei <mafteiandreiiulian@gmail.com>
2024-09-20 12:37:02 +0200

    sasa

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,6 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths:{
-			base: process.env.Node_ENV === 'production' ? '/verceltest-2' : '',
 			base: process.env.Node_ENV === 'production' ? '/verceltest-2.github.io' : '',
 		}
 	}
