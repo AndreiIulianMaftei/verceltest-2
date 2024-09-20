a6fcc2ac4861725329b30b89d0fd11b67f2c3b50
AndreiIulianMaftei <mafteiandreiiulian@gmail.com>
2024-09-20 14:58:27 +0200

    changed location of pics

--- svelte.config.js
+++ svelte.config.js
@@ -8,13 +8,21 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		prerender:{
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths:{
-			base: process.env.Node_ENV === 'production' ? '/verceltest-2.github.io' : '',
-		}
 	}
 };
 
